3b37e5bfba1938ec3f82e50714aebbebe3f56196
Demis Rincon <darmfma@gmail.com>
2021-07-07 17:15:14 -0500

    initialization

--- .next/static/chunks/pages/_app.js
+++ .next/static/chunks/pages/_app.js
@@ -8312,7 +8312,7 @@ module.exports = function(module) {
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! next-client-pages-loader?page=%2F_app&absolutePagePath=next%2Fdist%2Fpages%2F_app! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_app&absolutePagePath=next%2Fdist%2Fpages%2F_app!./");
-module.exports = __webpack_require__(/*! D:\Development\Practice\Senoritasonrisas\node_modules\next\dist\client\router.js */"./node_modules/next/dist/client/router.js");
+module.exports = __webpack_require__(/*! E:\Development\Practice\Senoritasonrisas\node_modules\next\dist\client\router.js */"./node_modules/next/dist/client/router.js");
 
 
 /***/ })
